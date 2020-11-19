d0d94913d7b86a2e23db9d097ef42afd1c5d5d02
mwj427 <35078769+mwj427@users.noreply.github.com>
2020-11-19 12:54:22 +0100

    Update structs.h

---
+++ structs.h
@@ -0,0 +1,9 @@
+typedef struct Car {
+  double speed;
+  double break;
+  double location;  /* så du ved hvor bilen er på broen */
+  double car_length;
+  double speed_limit;
+  double acceleration;
+  double safe_distance;
+}Car;
