4008dbe9fb22fa29f45ddda7faf965a7b32b5d78
mwj427 <35078769+mwj427@users.noreply.github.com>
2020-11-19 13:31:04 +0100

    fuck u i am done

--- structs.h
+++ structs.h
@@ -1,15 +1,15 @@
 
 typedef enum {green, yellow, red} Light_color;
-typedef enum {Bus, Car} Lane_type;
+typedef enum {Bus_lane, Car_lane} Lane_type;
 
-typedef struct Traffic_light {
+typedef struct  {
   Light_color light_color;
   double position;
 } Traffic_light;
 
-typedef struct Car {
+typedef struct {
   double speed;
-  double break;
+  double breaks;
   double location;  /* så du ved hvor bilen er på broen */
   double car_length;
   double speed_limit;
@@ -21,4 +21,3 @@ typedef struct {
 Lane_type lane_type; /* Type can be "Bus" or "Car" */
 double len; /* Length of road */
 } Road;
-
