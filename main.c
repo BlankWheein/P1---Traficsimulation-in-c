597daf0e132222881db4c30ac69ee5b1a8123612
ParkEunBlank <47646799+ParkEunBlank@users.noreply.github.com>
2020-11-19 13:08:47 +0100

    Update main.c

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <windows.h>
+
+#include "functions.h"
+#include "strcuts.h"
+
+
+
+
+int main(void) {
+
+
+
+return EXIT_SUCSSES;
+}
