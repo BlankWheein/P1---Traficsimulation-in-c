770efdd3443bf283c695ecbbc4838cd221987c44
Daniel <dbighi19@student.aau.dk>
2020-11-19 14:19:12 +0100

    Create nearest_trffic_light.c

---
+++ nearest_trffic_light.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "structs.h"
+
+Traffic_light get_nearest_traffic_light(Car car, Traffic_light lights[]) {
+  Traffic_light nearest_light = lights[0];
+  for (int i = 0; i < 2; i++) {
+
+  }
+}
+
+int main(void) {
+  Traffic_light Traffic_light1 = {green, 3};
+  Traffic_light Traffic_light2 = {red, 9};
+
+  Car car = {1, 1, 2, 1, 1, 1, 1};
+  Traffic_light lights = {Traffic_light1, Traffic_light2};
+  Traffic_light x = get_nearest_traffic_light(car, lights);
+  printf("This is the neareast traffic light: %lf", x.position);
+}
