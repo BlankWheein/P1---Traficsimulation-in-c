2b695d3b233581922c07850c16bc8304abd85a45
blankwheein <noobs7410@live.dk>
2020-11-23 20:53:33 +0100

    Update main.c

--- main.c
+++ main.c
@@ -3,13 +3,19 @@
 #include <time.h>
 #include <windows.h>
 
+
 #include "functions.h"
-#include "structs.h"
+
 
 
 
 
 int main(void) {
+
+Car car = {0, 0, 0, 0, 50, 4, 10, 1};
+Road road = {50, Car_lane, 670};
+print_car(car);
+
 
 
 return EXIT_SUCCESS;
