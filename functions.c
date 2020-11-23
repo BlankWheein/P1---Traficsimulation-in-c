658ee0b248d91428fa56150dc1d6673164173bd2
blankwheein <noobs7410@live.dk>
2020-11-23 20:53:09 +0100

    Added print_car

--- functions.c
+++ functions.c
@@ -1,6 +1,4 @@
-#include <stdio.h>
 #include "structs.h"
-
 Car accelerate_car(Car car, Road road){
   car.speed += car.acceleration;
   if (car.speed > car.speed_limit) {
@@ -12,13 +10,13 @@ Car accelerate_car(Car car, Road road){
   return car;
 }
 
+void print_car(Car car) {
+printf("Car(%d): Speed: %.2lf, breaks: %.2lf, location: %.2lf, lenght: %.2lf, speed_limit: %.1lf, acceleration: %.3lf, safe_distance: %.2lf\n", car.ID, car.speed, car.breaks, car.location, car.length, car.speed_limit, car.acceleration, car.safe_distance);
+
+
+}
+
 int check_light(Traffic_light light) {
   int drive = light.light_color == green ? 1 : 0;
   return drive;
-}
-
-
-int main (){
-
-  return 0;
 }
--- functions.h
+++ functions.h
@@ -1,3 +1,4 @@
-#include "functions.c"
+#include "structs.h"
+void print_car(Car car);
 Car accelerate_car(Car car, Road road);
 int check_light(Traffic_light light);
