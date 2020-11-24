f8b67a8f05443d95719117dc7aab0dbdc64857da
blankwheein <noobs7410@live.dk>
2020-11-24 19:33:45 +0100

    Added Prototypes

--- functions.h
+++ functions.h
@@ -1,13 +1,21 @@
 #ifndef FUNCTIONS
 #define FUNCTIONS
 
-
+Car set_car_acceleration(Car car);
+Car accelerate_car(Car car, Road road);
 Car drive(Car car, Car cars[], int cars_int, Road road);
+double kmt_to_ms(double x);
+Traffic_light create_light(Light_color color, double pos, int timer_green, int timer_red);
+Car set_safe_distance(Car car);
+int check_if_safe_distance(Car car, Car car_in_front);
 void print_car(Car car);
-Car accelerate_car(Car car, Road road);
-int check_light(Traffic_light light);
-Car accelerate_car(Car car, Road road);
-Car acceleration(Car car, Road road);
+void print_cars(Car cars[], int cars_int);
+char* color_to_string(Light_color color);
+Traffic_light count_timer(Traffic_light light);
+Traffic_light nearest_traffic_light(Car car, Traffic_light lights[], int lights_int);
+Car create_car(int id, int dist, double speed_limit_);
 Car get_nearest_car(Car car, Car cars[], int cars_int);
+int check_light(Traffic_light light);
+Road create_road(double speed_limit, Lane_type lane, double len);
 
 #endif
