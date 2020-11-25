7c63116928682d9e358a6c15d5712b63c7331bd3
ParkEunBlank <47646799+ParkEunBlank@users.noreply.github.com>
2020-11-25 11:12:18 +0100

    Update functions.c

--- functions.c
+++ functions.c
@@ -1,9 +1,6 @@
 #include "functions.h"
 
 Car set_car_acceleration(Car car){
-  car.speed_limit_time= 8.5;
-  /* Constant acceleraton formula. */
-  /*acceleration = 2(1-theta)(v_final - v_0) m/s / t_accTime s (m/s^2) */
   car.acceleration = (2 * (1 - (car.time_driving/car.speed_limit_time)) * (car.speed_limit - 0)) / car.speed_limit_time;
   return car;
 }
